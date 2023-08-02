f7d0d968c5e68ce66043141282994887416a0a7e
tsubasa-omi <info@jiyuna-design.com>
2023-08-02 22:18:26 +0900

    <変更>

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+
+    webpack: function(config) {
+        config.module.rules.push({
+            test: /\.md/,
+            use: "raw-loader"
+        })
+        return config
+    },
+}
 
 module.exports = nextConfig
